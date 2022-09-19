0a75395e4a6139892db8e797ed7980cfa3ce594b
Vu Quach <zed.quach@gmail.com>
2022-09-20 02:44:40 +0700

    init project

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>Hello CSCI E-39!</p>
+        <p>My name is Vu Quach</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
